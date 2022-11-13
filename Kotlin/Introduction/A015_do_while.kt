9caa84c82864a3ed6759e68f9b01f1a245dda1c5
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-13 00:57:18 -0600

    Create A015_do_while.kt

---
+++ Kotlin/Introduction/A015_do_while.kt
@@ -0,0 +1,11 @@
+fun main() {
+   // Do While loop execute a block of code repeatly as long as given condition
+   var x = 20
+    
+   do{
+       println("x = ${x++}")
+   }while(x < 30)
+   
+   println("Do While is done")
+   
+}
