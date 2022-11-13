0e1cbf5a0ba1962b957e2aab34ec2ed6676bb516
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-12 23:41:35 -0600

    Update A004_String_Interpolation.kt

--- Kotlin/Introduction/A004_String_Interpolation.kt
+++ Kotlin/Introduction/A004_String_Interpolation.kt
@@ -1,4 +1,8 @@
 fun main(){
+	val myStr = "Hello World" 
+    var firstCharInStr = myStr[0]
+	var lastCharInStr = myStr[myStr.length-1]
 
-
+    // Print Results
+    println("First Character: $firstCharInStr\n Last Character: $lastCharInStr \n String Length: ${myStr.length} letters")
 }
