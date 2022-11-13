8b3b74e330114948b9de75b1479b952ce89c21c0
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-13 09:12:58 -0600

    Create A019_null.kt

---
+++ Kotlin/Introduction/A019_null.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var nullableName : String? = null
+    //var nullableName : String? = "Maxwell"
+
+    
+    var lenNull = nullableName?.length
+    // The previous line is equivalent to next if else statement
+    /*
+    if(nullable != null){
+        lenNull = nullableName.length
+    }else{
+        null
+    }+
+    */
+    
+    
+    println("nullableName = $nullableName")
+    
+    // If we want to do something if the variable is not null we use ?.let{}
+    nullableName?.let{println(it.length)} 		// Print the length of the string
+    nullableName?.let{println(it.lowercase())}	// Chane the letters to lower case
+    
+}
