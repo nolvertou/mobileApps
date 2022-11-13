a7588c165f379313566e1cfb369dceb8b383846b
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-12 23:58:47 -0600

    Create A006_Comparison_Operators.kt

---
+++ Kotlin/Introduction/A006_Comparison_Operators.kt
@@ -0,0 +1,22 @@
+fun main(){
+	// Comparison Operators ( ==, !=, <, >, <=, >=)
+    var a = 1
+    var b = 2
+    var c = 1
+    
+    if(a == c){
+        println("a is equal to c")
+    }
+    
+    if(a != b){
+        println("a is different to b")
+    }
+    
+    if(a < b){
+        println("a is less than b")
+    }
+    
+    if(b > a){
+        println("b is greater than a")
+    }
+}
