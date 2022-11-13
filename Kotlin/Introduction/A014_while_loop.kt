e9b2f417b920de4eb4f010d2ab401ce2790aec8a
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-13 00:52:04 -0600

    Create A014_while_loop.kt

---
+++ Kotlin/Introduction/A014_while_loop.kt
@@ -0,0 +1,8 @@
+fun main() {
+   // While loop execute a block of code repeatly as long as given condition
+   var x = 1
+   while(x <= 10){
+       println("x = ${x++}")
+   } 
+   println("While loop is done")
+}
