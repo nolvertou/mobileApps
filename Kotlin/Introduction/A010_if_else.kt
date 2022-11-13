dc4d323400927d63dff1991341de540a70cb7a67
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-13 00:24:28 -0600

    Create A010_if_else.kt

---
+++ Kotlin/Introduction/A010_if_else.kt
@@ -0,0 +1,13 @@
+fun main() {
+   val age = 15
+    
+   if(age >= 21){
+       println("Now you may drink in USA")
+   }else if(age >= 18){
+       println( "You may vote now")
+   }else if(age >= 16){
+       println( "You may drive now")
+   }else{
+       println("You are too young")
+   }
+}
