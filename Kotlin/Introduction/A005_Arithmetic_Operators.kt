c1dfcebf87a390024b9ebbfcfc3fced3a8f82d45
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-12 23:51:39 -0600

    Create A005_Arithmetic_Operators.kt

---
+++ Kotlin/Introduction/A005_Arithmetic_Operators.kt
@@ -0,0 +1,39 @@
+fun main(){
+	// Arithmetic Operators (+, -, *, /, %)
+	// Addition
+	var a = 5 + 3		
+    // Result
+    println("a = $a")	// 8
+    
+    // Substraction
+	var b = 5 - 3		
+    // Result
+    println("b = $b")	// 2
+    
+    // Multiplication
+	var c = 5 * 3		
+    // Result
+    println("c = $c")	// 15
+    
+    // Division 1
+	var d = 5 / 3		
+    // Result
+    println("d = $d")	// 1
+    
+    
+    // Division 2
+	var e = 6 / 3		
+    // Result
+    println("e = $e")	// 2
+    
+    // Residue
+	var f = 6 % 3		
+    // Result
+    println("f = $f")	// 0
+    
+    // Residue
+	var g = 6 % 4		
+    // Result
+    println("g = $g")	// 2
+    
+}
