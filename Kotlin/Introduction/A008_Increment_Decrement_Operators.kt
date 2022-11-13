89ecf11292dd5645caf93db020fa7146d04eef2e
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-13 00:16:56 -0600

    Create A008_Increment_Decrement_Operators.kt

---
+++ Kotlin/Introduction/A008_Increment_Decrement_Operators.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var myNum = 0
+    
+    myNum++		// 1
+    myNum++		// 2
+    myNum++		// 3
+    ++myNum
+    
+    println("myNum = $myNum") 	// 4
+    
+    myNum-- 		
+   	println("myNum = $myNum")	// 3
+    --myNum
+    println("myNum = $myNum")	// 2
+}
