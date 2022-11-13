01d48a538677ae03f42be09f461c733e19412c0c
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-11-13 01:18:31 -0600

    Create A018_function_parameter.kt

---
+++ Kotlin/Introduction/A018_function_parameter.kt
@@ -0,0 +1,11 @@
+fun main(){
+    // Argument Input
+    var result = addUp(1,16)
+    println("result = $result")
+    
+}
+
+// Parameter Input
+fun addUp(a: Int, b: Int): Int{
+    return a + b	// Output
+}
