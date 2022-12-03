4ce5a0df0dde773f99ea03f12d38e139899254cd
Nolverto Urias Obeso <nolvertou@gmail.com>
2022-12-03 12:44:08 -0600

    Create A031_Finally_block.kt

---
+++ Kotlin/Introduction/A031_Finally_block.kt
@@ -0,0 +1,18 @@
+// Goal: 			Learn how to use multiple catch block
+// Creation Date: 	12/03/2022
+// Created by: 		Engineer Nolverto Urias Obeso
+// 
+
+
+fun main(agrs: Array<String>){
+	try{
+        val data = 10/5
+        println(data)
+    }catch(e: NullPointerException){
+        println(e)
+    }finally{
+        println("finally block always executes")
+    }
+    
+    println("bellow code...")
+}
